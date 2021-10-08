08a97b948f518f05d5f7db6fb2a1ad6f278e5bea
Julien Cohen Solal <julien_cs@hotmail.com>
2021-10-08 16:09:12 +0200

    Added optimizers from TF and TFA

--- src/utils/optimizer_handler.py
+++ src/utils/optimizer_handler.py
@@ -1,6 +1,7 @@
 import utils.c_logging as c_logging
 
 from tensorflow.keras import optimizers, mixed_precision
+import tensorflow_addons as tfa
 
 LOG = c_logging.getLogger(__name__)
 
@@ -14,8 +15,18 @@ def get_optimizer(conf, is_warmup=False):
     optimizer = None
     if conf.optimizer == "Adam": # = RMSprop with momentum
         optimizer = optimizers.Adam(learning_rate=lr)
+    elif conf.optimizer == "AdamW": # = Adam with weight decay
+        optimizer = tfa.optimizers.AdamW(learning_rate=lr, weight_decay=conf.weight_decay)
+    elif conf.optimizer == "LAMB": # = Layer-wise Adaptive Moments
+        optimizer = tfa.optimizers.LAMB(learning_rate=lr, weight_decay_rate=conf.weight_decay)
     elif conf.optimizer == "Nadam": # = Adam with Nesterov momentum
         optimizer = optimizers.Nadam(learning_rate=lr)
+    elif conf.optimizer == "RectifiedAdam": # = Adam with rectified adaptive learning rate to have a consistent variance
+        optimizer = tfa.optimizers.RectifiedAdam(learning_rate=lr, weight_decay=conf.weight_decay)
+    elif conf.optimizer == "RMSprop":
+        optimizer = optimizers.RMSprop(learning_rate=lr)
+    elif conf.optimizer == "SGD":
+        optimizer = optimizers.SGD(learning_rate=lr)
     else:
         LOG.error("Unknown optimizer, quitting")
     
