80e0976f459b2d49b33fc969085878a52399c245
Julien Cohen Solal <julien_cs@hotmail.com>
2021-10-17 20:42:38 +0200

    Refacto callbacks handling in a separate file

--- src/train_classif.py
+++ src/train_classif.py
@@ -10,8 +10,8 @@ from sklearn.utils import shuffle
 import tensorflow as tf
 
 from projects import cassava, flowers
-from utils import augmentations, c_logging, config, cv, data, loss_handler, lr_handler
-from utils import metric_handler, mlflow_log, network_handler, optimizer_handler, startup, visualize
+from utils import augmentations, c_logging, config, cv, data, loss_handler, lr_handler, metric_handler
+from utils import mlflow_log, network_handler, optimizer_handler, startup, training, visualize
 
 
 LOG = c_logging.getLogger(__name__)
@@ -200,43 +200,8 @@ if __name__ == '__main__' :
             sys.exit()
         model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
-        # Set up early stopping
-        callbacks = []
-        if conf.early_stopping_epochs > 0:
-            LOG.debug("Set up early stopping")
-            es_callback = tf.keras.callbacks.EarlyStopping(
-                monitor='val_loss', patience=conf.early_stopping_epochs, 
-                restore_best_weights=conf.restore_best_after_training)
-            callbacks.append(es_callback)
-
-        # Set up learning rate
-        if conf.lr is not None:
-            LOG.debug("Set up learning rate")
-            lr = lr_handler.get_learning_rate(conf)
-            if lr is None:
-                sys.exit()
-            lr_callback = tf.keras.callbacks.LearningRateScheduler(lr)
-            callbacks.append(lr_callback)
-
-        # Plug to Tensorboard
-        if conf.tensorboard:
-            LOG.debug("Plug Tensorboard")
-            tensorboard_dir = experiment_dir + "tensorboard/fold_" + str(fold_idx) + "/"
-            os.makedirs(tensorboard_dir)
-            tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_dir)
-            callbacks.append(tensorboard_callback)
-
-        # Set up checkpoints
-        if conf.checkpoints:
-            LOG.debug("Set up checkpoints")
-            checkpoint_dir = experiment_dir + "checkpoints/fold_" + str(fold_idx) + "/"
-            os.makedirs(checkpoint_dir)
-            cp_callback = tf.keras.callbacks.ModelCheckpoint(
-                filepath=checkpoint_dir + conf.base + "-{epoch:02d}-{val_loss:.4f}.ckpt",
-                monitor="val_loss", 
-                save_best_only=True,
-                save_weights_only=True)
-            callbacks.append(cp_callback)
+        # Set up callbacks
+        callbacks = training.set_callbacks(conf, experiment_dir, fold_idx)
 
         # Set up MLFlow tracking
         LOG.debug("Set up MLFlow tracking")
---
+++ src/utils/training.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import tensorflow as tf
+
+import lr_handler
+import utils.c_logging as c_logging
+
+LOG = c_logging.getLogger(__name__)
+
+
+def set_callbacks(conf, experiment_dir, fold_idx):
+    # Set up early stopping
+    callbacks = []
+    if conf.early_stopping_epochs > 0:
+        LOG.debug("Set up early stopping")
+        es_callback = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss', patience=conf.early_stopping_epochs, 
+            restore_best_weights=conf.restore_best_after_training)
+        callbacks.append(es_callback)
+
+    # Set up learning rate
+    if conf.lr is not None:
+        LOG.debug("Set up learning rate")
+        lr = lr_handler.get_learning_rate(conf)
+        if lr is None:
+            sys.exit()
+        lr_callback = tf.keras.callbacks.LearningRateScheduler(lr)
+        callbacks.append(lr_callback)
+
+    # Plug to Tensorboard
+    if conf.tensorboard:
+        LOG.debug("Plug Tensorboard")
+        tensorboard_dir = experiment_dir + "tensorboard/fold_" + str(fold_idx) + "/"
+        os.makedirs(tensorboard_dir)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_dir)
+        callbacks.append(tensorboard_callback)
+
+    # Set up checkpoints
+    if conf.checkpoints:
+        LOG.debug("Set up checkpoints")
+        checkpoint_dir = experiment_dir + "checkpoints/fold_" + str(fold_idx) + "/"
+        os.makedirs(checkpoint_dir)
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_dir + conf.base + "-{epoch:02d}-{val_loss:.4f}.ckpt",
+            monitor="val_loss", 
+            save_best_only=True,
+            save_weights_only=True)
+        callbacks.append(cp_callback)
+
+    return callbacks
